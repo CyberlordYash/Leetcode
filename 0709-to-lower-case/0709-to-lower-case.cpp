b8fd16e2d678cbb4e93741501c68faa6f1cb8458
Yash Sachan <95024305+CyberlordYash@users.noreply.github.com>
2022-10-25 22:20:58 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (53.93%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string toLowerCase(string s) {
         int size=s.size();int n;
-        for(int i=0;i<size;i++)
+        for(int i=0;i<size;++i)
         {   n=s[i];
             if(n<=90 && n>=65){n=n+32;s[i]=n;}
             
